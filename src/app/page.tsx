dbac06f1a607917f26cab41b4017bb2bff29b3ca
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-11-07 09:14:43 +0100

    added hero image to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hejsan</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam
-        asperiores alias reprehenderit nostrum dolores natus nobis ut
-        praesentium quo excepturi, recusandae quis maxime illo, exercitationem
-        vel aspernatur! Repellendus, maiores.
-      </p>
+    <div className="w-full h-screen flex items-center justify-center">
+      <img src="/rentaplace.png" alt="Rentaplace hero" className="object-cover w-full h-full" />
     </div>
   );
 }
